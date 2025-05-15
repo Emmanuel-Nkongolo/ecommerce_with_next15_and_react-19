efb21c5060f306fef09e877828db5074d0c2e7be
Emmanuel-Nkongolo <nkongolo64@gmail.com>
2025-05-15 19:07:05 +0200

    Sign Up zod & Action

--- lib/validators.ts
+++ lib/validators.ts
@@ -27,3 +27,14 @@ export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
   password: z.string().min(5, "Password must be at least 5 characters"),
 });
+
+// Shcema for signing up a user
+export const signUpFormSchema = z.object({
+  name: z.string().min(3, 'Name must be at least 3 characters'),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(5, "Password must be at least 5 characters"),
+  confirmPassword: z.string().min(5, "Password must be at least 5 characters"),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords don't match",
+  path: ['confirmPassword'],
+});
