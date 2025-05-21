e382084d683c63deee634b7069b4edcf8dd1bf5c
Emmanuel <nkongolo64@gmail.com>
2025-05-21 17:21:17 +0200

    Get Item from Cart and from db

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -2,8 +2,10 @@
 
 import { cookies } from "next/headers";
 import { CartItem } from "@/types";
-import { formatError } from "../utils";
+import { convertToPlainObject, formatError } from "../utils";
 import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -15,10 +17,23 @@ export async function addItemToCart(data: CartItem) {
     const session = await auth();
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
+    // Get cart
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
     // TESTING
     console.log({
       "Session Cart ID": sessionCartId,
       "User ID": userId,
+      "Item Requested": item,
+      "Product Found": product,
     });
 
     return {
@@ -31,4 +46,31 @@ export async function addItemToCart(data: CartItem) {
       message: formatError(error),
     };
   }
+}
+
+export async function getMyCart() {
+  // Check for cart cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Cart session not found");
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  //Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // Convert decimals and return
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
