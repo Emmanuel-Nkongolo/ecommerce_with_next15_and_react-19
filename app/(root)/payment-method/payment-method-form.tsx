b5041bf636e012725b0ae8cfc01a2a1ea1ce995e
Emmanuel <nkongolo64@gmail.com>
2025-05-27 20:56:08 +0200

    Create payment method page and a form page

---
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -0,0 +1,22 @@
+"use client";
+//
+
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useTransition } from "react";
+import { paymentMethodSchema } from "@/lib/validators";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+
+const PaymentMethodForm = ({
+  preferredPaymentMethod,
+}: {
+  preferredPaymentMethod: string | null;
+}) => {
+  return (
+    <>
+      <CheckoutSteps current={2} />
+    </>
+  );
+};
+
+export default PaymentMethodForm;
